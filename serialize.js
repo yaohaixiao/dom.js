045d8e4abbd8a4fd33d4f3d84de69fd26df11c8a
Yaohaixiao <yaohaixiao@gmail.com>
2023-11-01 19:44:12 +0800

    docs: 完成 serialize() 方法的单测合 API 文档编写；

--- serialize.js
+++ serialize.js
@@ -1,10 +1,32 @@
+import isElement from './isElement'
+
 /**
  *
- * @param form
- * @return {string}
+ * ========================================================================
+ * @method serialize
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/FormData
+ * @param {HTMLElement} formElement
  */
-const serialize = (form) => {
-  return new URLSearchParams(new FormData(form)).toString()
+const serialize = (formElement, isParams = false) => {
+  let serialized = {}
+  let data
+
+  if (!isElement(formElement) || formElement.tagName.toLowerCase() !== 'form') {
+    return false
+  }
+
+  data = new FormData(formElement)
+
+  if (isParams) {
+    serialized = new URLSearchParams(data).toString()
+  } else {
+    for (const [key, value] of data) {
+      serialized[key] = value
+    }
+  }
+
+  return serialized
 }
 
 export default serialize
