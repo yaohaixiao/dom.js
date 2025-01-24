dc7c0504edcdaa1634f66ce6c66915cef663b582
Yaohaixiao <yaohaixiao@gmail.com>
2025-01-24 11:47:17 +0800

    fix: 修复 replace() 方法中使用 html 字符串作为 replacement 参数时，会重复替换2个 DOM 元素的问题

--- replace.js
+++ replace.js
@@ -3,8 +3,8 @@ import isFunction from './utils/types/isFunction'
 import isDOM from './isDOM'
 import isElement from './isElement'
 import insertAfter from './insertAfter'
-import insertHTMLAfterEnd from './insertHTMLAfterEnd'
 import hide from './hide'
+import html from './html'
 
 /**
  * 用指定 DOM 节点或者将 HTML 字符转化成节点替换 reference 节点
@@ -12,37 +12,42 @@ import hide from './hide'
  * @method replace
  * @since 1.1.0
  * @see https://developer.mozilla.org/zh-CN/docs/Web/API/Element/replaceWith
- * @param {HTMLElement|String} el
- * @param {HTMLElement} reference
+ * @param {HTMLElement|String|Function} replacement
+ * @param {HTMLElement} el
  * @return {Element|null}
  */
-const replace = (el, reference) => {
+const replace = (replacement, el) => {
   let $replace
   let $parent
 
   /* istanbul ignore else */
-  if (!isElement(reference) || (!isDOM(el) && !isHTML(el))) {
+  if (
+    !isElement(el) ||
+    (!isDOM(replacement) && !isHTML(replacement) && isFunction(replacement))
+  ) {
     return null
   }
 
-  $parent = reference.parentNode
+  $parent = el.parentNode
 
   if (!$parent) {
     return null
   }
 
-  hide(reference)
+  hide(el)
 
-  if (isDOM(el)) {
-    $replace = insertAfter(el, reference)
-  } else {
-    $replace = insertHTMLAfterEnd(reference, el)
+  if (isDOM(replacement)) {
+    $replace = insertAfter(replacement, el)
+  } else if (isHTML(replacement)) {
+    $replace = html(replacement)
+  } else if (isFunction(replacement)) {
+    $replace = replacement()
   }
 
-  if (isFunction(reference.replaceWith)) {
-    reference.replaceWith($replace)
+  if (isFunction(el.replaceWith)) {
+    el.replaceWith($replace)
   } else {
-    reference.remove()
+    el.remove()
   }
 
   return $replace
